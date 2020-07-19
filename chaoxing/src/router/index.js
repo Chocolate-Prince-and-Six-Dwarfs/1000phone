055b0b439b3ede5255bb1c6c2e13a6169e49a28a
dreamone11 <dreamone11@outlook.com>
2020-07-19 12:55:51 +0800

    项目架构
    2nd commit

--- chaoxing/src/router/index.js
+++ chaoxing/src/router/index.js
@@ -1,6 +1,10 @@
 import Vue from 'vue'
 import Router from 'vue-router'
-import HelloWorld from '@/components/HelloWorld'
+import Home from '@/components/home'
+import Information from '@/components/information'
+import Note from '@/components/note'
+import My from '@/components/my'
+import MyAccount from '@/components/myAccount'
 
 Vue.use(Router)
 
@@ -8,8 +12,28 @@ export default new Router({
   routes: [
     {
       path: '/',
-      name: 'HelloWorld',
-      component: HelloWorld
-    }
+      name: 'Home',
+      component: Home
+    },
+    {
+      path: '/information',
+      name: 'Informationome',
+      component: Information
+    },
+    {
+      path: '/note',
+      name: 'Note',
+      component: Note
+    },
+    {
+      path: '/my',
+      name: 'My',
+      component: My
+    },
+    {
+      path: '/myAccount',
+      name: 'MyAccount',
+      component: MyAccount
+    },
   ]
 })
