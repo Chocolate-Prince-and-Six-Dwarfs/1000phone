05298cd3c02baa8650c716a7fa55b96bce9f26ec
dreamone11 <dreamone11@outlook.com>
2020-07-19 19:25:34 +0800

    index.js修改

--- chaoxing/src/components/myAccount.vue
+++ chaoxing/src/components/myAccount.vue
@@ -1,5 +1,23 @@
 <template>
-  
+  <div class="account">
+      <div class="account-head">
+          <div class="drawback">
+              <img class="drawback-img" src="../assets/img/jt.png" alt="">
+          </div>
+          <div class="head-text">账号管理</div>
+      </div>
+      <div class="account-main">
+          <div class="avater"></div>
+          <div class="name"></div>
+          <div class="sex"></div>
+          <div class="description"></div>
+          <div class="phoneNum"></div>
+          <div class="work"></div>
+          <div class="wallet"></div>
+          <div class="cancel"></div>
+      </div>
+      <div class="account-bottom"></div>
+  </div>
 </template>
 
 <script>
@@ -9,5 +27,17 @@ export default {
 </script>
 
 <style>
-
+    .account-head{
+        width: 100%;
+        height: 65px;
+        display: flex;
+        align-items: center;
+    }
+    .drawback{
+        float: left;
+    }
+    .drawback-img{
+        width: 25px;
+        height: 25px;
+    }
 </style>
